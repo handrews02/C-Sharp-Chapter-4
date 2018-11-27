49922c400bc3179074d2e5080761dfc6304dcaa4
Hannah Andrews <handrews02@users.noreply.github.com>
2018-11-27 12:32:30 -0600

    update text

---
+++ Twitter/Twitter/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Twitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTwittterMsg = new System.Windows.Forms.TextBox();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.btnPost = new System.Windows.Forms.Button();
+            this.lblPostedMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTwittterMsg
+            // 
+            this.txtTwittterMsg.Location = new System.Drawing.Point(263, 40);
+            this.txtTwittterMsg.Multiline = true;
+            this.txtTwittterMsg.Name = "txtTwittterMsg";
+            this.txtTwittterMsg.Size = new System.Drawing.Size(250, 135);
+            this.txtTwittterMsg.TabIndex = 0;
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCounter.Location = new System.Drawing.Point(333, 196);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(112, 30);
+            this.lblCounter.TabIndex = 1;
+            // 
+            // btnPost
+            // 
+            this.btnPost.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnPost.Font = new System.Drawing.Font("Monotxt_IV25", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPost.Location = new System.Drawing.Point(346, 242);
+            this.btnPost.Name = "btnPost";
+            this.btnPost.Size = new System.Drawing.Size(87, 30);
+            this.btnPost.TabIndex = 2;
+            this.btnPost.Text = "Post!";
+            this.btnPost.UseVisualStyleBackColor = false;
+            this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
+            // 
+            // lblPostedMsg
+            // 
+            this.lblPostedMsg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPostedMsg.Location = new System.Drawing.Point(333, 298);
+            this.lblPostedMsg.Name = "lblPostedMsg";
+            this.lblPostedMsg.Size = new System.Drawing.Size(112, 30);
+            this.lblPostedMsg.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 386);
+            this.Controls.Add(this.lblPostedMsg);
+            this.Controls.Add(this.btnPost);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.txtTwittterMsg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTwittterMsg;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Button btnPost;
+        private System.Windows.Forms.Label lblPostedMsg;
+    }
+}
+
---
+++ Twitter/Twitter/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Twitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPost_Click(object sender, EventArgs e)
+        {
+            string message;
+            const int MAX = 140;
+
+            message = txtTwittterMsg.Text;
+
+            if (message.Length > MAX)
+            {
+                lblCounter.Text = message.Length + " OVER LIMIT";
+                lblCounter.ForeColor = Color.FromArgb(255,0,0);
+            }
+            else
+            {
+                lblCounter.Text = message.Length + " UNDER LIMIT";
+                lblPostedMsg.Text = "Posted to Twitter!";
+            }
+        }
+    }
+}
