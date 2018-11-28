bff7c6577c1b8735390590973dbdcd8f25a29af8
Hannah Andrews <handrews02@users.noreply.github.com>
2018-11-28 12:33:05 -0600

    Update Form1.Designer.cs

--- RockPaperScissors/RockPaperScissors/Form1.Designer.cs
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.btnPaper = new System.Windows.Forms.Button();
             this.btnScissors = new System.Windows.Forms.Button();
             this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnRock
@@ -77,11 +78,22 @@
             this.lblResult.Size = new System.Drawing.Size(347, 92);
             this.lblResult.TabIndex = 3;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotxt_IV25", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(614, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ROCK, PAPER, SCISSORS!";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1069, 604);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.btnScissors);
             this.Controls.Add(this.btnPaper);
@@ -89,6 +101,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -98,6 +111,7 @@
         private System.Windows.Forms.Button btnPaper;
         private System.Windows.Forms.Button btnScissors;
         private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
     }
 }
 
