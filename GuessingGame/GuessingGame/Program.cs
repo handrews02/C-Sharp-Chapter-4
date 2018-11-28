28c5aacba1a6691c62162a111c7ec0490b0cae56
Hannah Andrews <handrews02@users.noreply.github.com>
2018-11-28 12:05:44 -0600

    Update Program.cs

--- GuessingGame/GuessingGame/Program.cs
+++ GuessingGame/GuessingGame/Program.cs
@@ -6,7 +6,35 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            
+            int guess;
+            string guessString;
+            int min = 1;
+            int max = 11;
+            string result = null;
+
+            Random ranNumberGenerator = new Random();
+            int randomNumber;
+            randomNumber = ranNumberGenerator.Next(min, max);
+
+            Console.WriteLine("Enter a number between 1 and 10 >> ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess == randomNumber)
+            {
+                Console.WriteLine("You win! You guessed " + randomNumber);
+            }
+            else
+            {
+                Console.WriteLine("ERROR!");
+            }
         }
     }
 }
